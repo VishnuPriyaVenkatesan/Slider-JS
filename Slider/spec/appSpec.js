f32afeb7e510fe069242d9dc0bd023e00cb2840f
VishnuPriyaVenkatesan <vishnupriyav16@gmail.com>
2021-11-09 11:11:55 +0530

    src code

---
+++ Slider/spec/appSpec.js
@@ -0,0 +1,20 @@
+describe("getAuthorNameIfDataisNull", () => {
+  it("returns empty string if first parameter is null", () => {
+    var actual = getAuthorName(null, 0);
+    expect(actual).toBe("");
+  });
+})
+
+describe("getAuthorNameIfIdNotExists", () => {
+  it("returns empty string if ID not Exists", () => {
+    var actual = getAuthorName([{"format":"jpeg","width":5760,"height":3840,"filename":"1005.jpeg","id":1005,"author":"Matthew Wiebe","author_url":"https://unsplash.com/photos/tBtuxtLvAZs","post_url":"https://unsplash.com/photos/tBtuxtLvAZs"}], 234);
+    expect(actual).toBe("");
+  });
+})
+
+describe("getAuthorNameIfIdExists", () => {
+  it("returns appropriate author name if ID Exists", () => {
+    var actual = getAuthorName([{"format":"jpeg","width":5626,"height":3635,"filename":"1000.jpeg","id":1000,"author":"Lukas Budimaier","author_url":"https://unsplash.com/photos/6cY-FvMlmkQ","post_url":"https://unsplash.com/photos/6cY-FvMlmkQ"}], 1000);
+    expect(actual).toBe("Lukas Budimaier");
+  });
+})
---
+++ Slider/src/app.js
@@ -0,0 +1,87 @@
+$(document).ready(function ($) {
+
+  $.ajax({
+    headers: {
+      "Accept": "application/json",
+      "Content-type": "application/json"
+    },
+    url: 'https://picsum.photos/list',
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      loadImages(data);
+    },
+    error: function (error) {
+      console.log(`Error ${error}`);
+    }
+  });
+
+});
+
+function loadImages(data) {
+  var x = "", i = 1;
+  while (i <= 10) {
+    var j = getRandomInt(0, 1000);
+    var author = getAuthorName(data, j);
+    if (author.length > 0) {
+      i++;
+      x = x + '<li><div class="author">' + author + '</div><img src="https://picsum.photos/200/300?image=' + j + '"></li>';
+    }
+  }
+  document.getElementById("list").innerHTML = x;
+  initSlider();
+};
+
+function getAuthorName(data, j) {
+  var name = "";
+  if (typeof (data) !== "undefined" && data !== null) {
+    for (i = 0; i < data.length; i++) {
+      if (data[i].id == j) {
+        name = data[i].author;
+        return name;
+      }
+    }
+  }
+  return name;
+};
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+};
+
+function initSlider() {
+
+  var slideCount = $('#slider ul li').length;
+  var slideWidth = $('#slider ul li').width();
+  var sliderUlWidth = slideCount * slideWidth;
+
+  $('#slider ul').css({ width: sliderUlWidth, marginLeft: - 15 });
+
+  $('#slider ul li:last-child').prependTo('#slider ul');
+
+  function moveLeft() {
+    $('#slider ul').animate({
+      left: + slideWidth
+    }, 200, function () {
+      $('#slider ul li:last-child').prependTo('#slider ul');
+      $('#slider ul').css('left', '');
+    });
+  };
+
+  function moveRight() {
+    $('#slider ul').animate({
+      left: - slideWidth
+    }, 200, function () {
+      $('#slider ul li:first-child').appendTo('#slider ul');
+      $('#slider ul').css('left', '');
+    });
+  };
+
+  $('a.control_prev').click(function () {
+    moveLeft();
+  });
+
+  $('a.control_next').click(function () {
+    moveRight();
+  });
+};
